745077a9a0e2929d41494da415058dfdacb72492
Ritesh <riteshkumar19331@gmail.com>
2023-02-01 12:57:00 +0530

    assingment submitted

---
+++ Ritesh_Kumar_Coding/Prospecta/src/main/java/com/prospecta/Dto/EntryDto.java
@@ -0,0 +1,13 @@
+package com.prospecta.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class EntryDto {
+    private String title;
+    private String description;
+}
